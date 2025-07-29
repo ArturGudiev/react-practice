664c583f16263c1311922e1d8a4498deaa918699
Artur Gudiev <arturgudiev93@gmail.com>
2025-07-29 11:42:22 +0300

    3q

---
+++ src/Tickets/Component3.js
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+
+
+const Component3 = () => {
+    const counter = 0;
+    return <button>Press to increatse the counter {counter}</button>;
+};
+export default Component3;
