a9d4e094374e76e01481eefbafb51cfb38741b20
Artur Gudiev <arturgudiev93@gmail.com>
2025-07-29 11:13:59 +0300

    Component1

--- src/Component1.js
+++ src/Component1.js
@@ -1,8 +1,12 @@
+/*
+    Раскомментируй 2 квадрата и исправь ошибку
+*/
 // src/Component1.js
 import React from 'react';
 
 const Component1 = () => {
-    return <h1>This is Component 1</h1>;
+    return <div>Раскомментируй 2 квадрата и исправь ошибку</div>
+    // return <button className="square">X</button><button className="square">X</button>;
 };
 
 export default Component1;
