27670929e4946f96ca2d424b2a292a39415b0cb4
Artur Gudiev <arturgudiev93@gmail.com>
2025-07-29 11:28:47 +0300

    c2: q

---
+++ src/Tickets/Component2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Component2 = () => {
+    function f() {
+        alert('Hello there');
+    }
+    return <button>Press to show an alert</button>;
+};
+
+export default Component2;
+
